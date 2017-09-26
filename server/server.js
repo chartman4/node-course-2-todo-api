896ea162170a78bb0e47a73845e483b6f94fa005
chartman4 <chartman4@hotmail.com>
2017-09-26 14:38:06 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
+
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
@@ -28,7 +30,35 @@ app.get('/todos', (req,res) => {
   }, (e) => {
     res.status(400).send(e);
   });
-})
+});
+
+//GET /todos/1234123)
+app.get('/todos/:id', (req, res) => {
+  var id =req.params.id;
+  //validate ID using isValid
+    // 404 - send empty 
+  if (!ObjectID.isValid(id)) {
+    return es.status(404).send();
+  };
+
+    // findById 
+  Todo.findById(id).then((todo) => {
+  // success
+    if (!todo) {
+      return res.status(404).send();
+    }
+      //if todo - send it back
+      //if no todo - send back 404 with empty body
+      res.send({todo});
+  // error
+      // 400 - 
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+   
+});
+
 app.listen(3000, () => {
   console.log('Started on port 3000');
 });
