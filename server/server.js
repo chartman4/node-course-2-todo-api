9a3915d6588ac2867ab782dc1241054f3433ffa4
chartman4 <chartman4@hotmail.com>
2017-09-29 07:47:18 -0500

    Add PATCH/todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-const {ObjectID} = require('mongodb');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -25,9 +25,9 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req,res) => {
+app.get('/todos', (req, res) => {
   Todo.find().then((todos) => {
-    res.send({todos});
+    res.send({ todos });
   }, (e) => {
     res.status(400).send(e);
   });
@@ -35,56 +35,85 @@ app.get('/todos', (req,res) => {
 
 //GET /todos/1234123)
 app.get('/todos/:id', (req, res) => {
-  var id =req.params.id;
+  var id = req.params.id;
   //validate ID using isValid
-    // 404 - send empty 
+  // 404 - send empty 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   };
 
-    // findById 
+  // findById 
   Todo.findById(id).then((todo) => {
-  // success
+    // success
     if (!todo) {
       return res.status(404).send();
     }
-      //if todo - send it back
-      //if no todo - send back 404 with empty body
-      res.send({todo});
-  // error
-      // 400 - 
+    //if todo - send it back
+    //if no todo - send back 404 with empty body
+    res.send({ todo });
+    // error
+    // 400 - 
   }).catch((e) => {
     res.status(400).send();
   })
 
-   
+
 });
 
-app.delete('/todos/:id', (req,res) => {
-  var id =req.params.id;
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
   //validate ID using isValid
-    // 404 - send empty 
+  // 404 - send empty 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   };
   Todo.findByIdAndRemove(id).then((todo) => {
-        //if no todo - send back 404 with empty body
-      if (!todo) {
+    //if no todo - send back 404 with empty body
+    if (!todo) {
 
+      return res.status(404).send();
+    }
+    // success
+    //if todo - send it back
+    res.status(200).send({ todo });
+    // error 400 - 
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed) {
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+    .then((todo) => {
+      if (!todo) {
         return res.status(404).send();
       }
-    // success
-        //if todo - send it back
-        res.status(200).send({todo});
-    // error 400 - 
+      res.send({ todo });
+
     }).catch((e) => {
       res.status(400).send();
     })
-
 });
+
 
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
 
-module.exports = {app}
+module.exports = { app }
