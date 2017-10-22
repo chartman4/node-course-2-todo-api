261a6149131a60180c9a2ce69bd0837c18a25331
chartman4 <chartman4@hotmail.com>
2017-10-22 18:42:32 -0500

    Associate users with todos

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const { ObjectID } = require('mongodb');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
+var { authenticate } = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT;
@@ -17,9 +17,10 @@ const port = process.env.PORT;
 // middleware for express
 app.use(bodyParser.json());
 
-app.post('/todos', (req, res) => {
+app.post('/todos', authenticate, (req, res) => {
   var todo = new Todo({
-    text: req.body.text
+    text: req.body.text,
+    _creator: req.user._id
   });
 
   todo.save().then((doc) => {
@@ -29,8 +30,10 @@ app.post('/todos', (req, res) => {
   });
 });
 
-app.get('/todos', (req, res) => {
-  Todo.find().then((todos) => {
+app.get('/todos', authenticate, (req, res) => {
+  Todo.find({
+    _creator: req.user._id
+  }).then((todos) => {
     res.send({ todos });
   }, (e) => {
     res.status(400).send(e);
@@ -38,7 +41,7 @@ app.get('/todos', (req, res) => {
 });
 
 //GET /todos/1234123)
-app.get('/todos/:id', (req, res) => {
+app.get('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   //validate ID using isValid
   // 404 - send empty 
@@ -47,7 +50,10 @@ app.get('/todos/:id', (req, res) => {
   };
 
   // findById 
-  Todo.findById(id).then((todo) => {
+  Todo.findOne({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     // success
     if (!todo) {
       return res.status(404).send();
@@ -64,14 +70,17 @@ app.get('/todos/:id', (req, res) => {
 
 });
 
-app.delete('/todos/:id', (req, res) => {
+app.delete('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   //validate ID using isValid
   // 404 - send empty 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   };
-  Todo.findByIdAndRemove(id).then((todo) => {
+  Todo.findOneAndRemove({
+    _id: id,
+    _creator: req.user._id
+  }).then((todo) => {
     //if no todo - send back 404 with empty body
     if (!todo) {
 
@@ -88,14 +97,14 @@ app.delete('/todos/:id', (req, res) => {
 
 
 
-app.patch('/todos/:id', (req, res) => {
+app.patch('/todos/:id', authenticate, (req, res) => {
   var id = req.params.id;
   var body = _.pick(req.body, ['text', 'completed']);
 
   if (!ObjectID.isValid(id)) {
     return res.status(404).send();
   }
-
+  
   if (_.isBoolean(body.completed) && body.completed) {
     body.completedAt = new Date().getTime();
   } else {
@@ -103,7 +112,7 @@ app.patch('/todos/:id', (req, res) => {
     body.completedAt = null;
   }
 
-  Todo.findByIdAndUpdate(id, { $set: body }, { new: true })
+  Todo.findOneAndUpdate({_id : id, _creator: req.user._id}, { $set: body }, { new: true })
     .then((todo) => {
       if (!todo) {
         return res.status(404).send();
@@ -128,7 +137,7 @@ app.post('/users', (req, res) => {
     res.status(400).send(e);
   })
 });
-  
+
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
 });
@@ -140,7 +149,7 @@ app.post('/users/login', (req, res) => {
     // res.send(user);
     return user.generateAuthToken().then((token) => {
       res.header('x-auth', token).send(user);
-      
+
     })
   }).catch((e) => {
     res.status(400).send();
